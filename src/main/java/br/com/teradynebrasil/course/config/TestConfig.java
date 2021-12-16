d1ea6312fc3117c833f7390ae11fa7c806aa1f10
Edilson Prudencio <edilson.prudencio@gmail.com>
2021-12-16 15:07:42 -0300

    Payment, one-to-one association

--- src/main/java/br/com/teradynebrasil/course/config/TestConfig.java
+++ src/main/java/br/com/teradynebrasil/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import br.com.teradynebrasil.course.entities.Category;
 import br.com.teradynebrasil.course.entities.Order;
 import br.com.teradynebrasil.course.entities.OrderItem;
+import br.com.teradynebrasil.course.entities.Payment;
 import br.com.teradynebrasil.course.entities.Product;
 import br.com.teradynebrasil.course.entities.User;
 import br.com.teradynebrasil.course.entities.enums.OrderStatus;
@@ -98,6 +99,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3,p3,2,p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	
 		
